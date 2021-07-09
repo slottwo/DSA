822fb9e929d05711fc09726d3a859c33c97b2fef
Pedro [slottwo] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2021-07-09 20:08:17 -0300

    directory restructure

--- matrix/matrix.h
+++
@@ -1,8 +0,0 @@
-typedef struct matrix Matrix;
-
-Matrix* create_matrix (int, int);
-void free_matrix (Matrix*);
-float access_matrix (Matrix*, int, int);
-void attach_matrix (Matrix*, int, int, float);
-int rows (Matrix*);
-int columns (Matrix*);
